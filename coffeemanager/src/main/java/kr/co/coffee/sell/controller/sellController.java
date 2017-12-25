f86b0cf3aef68381e1d411a80d2831ea3892f5dd
Kim Young Seop <kkimm87@gmail.com>
2017-12-26 00:47:34 +0900

    쿼리 list 전송 추가
    mybatis에서 가지고 온 list를 view로 전송 - 초기에 메소드를 따로 만들었으나 알수없는 오류가 생겨서 한 기존
    메소드에 등록, rest방식은 아님..

--- coffeemanager/src/main/java/kr/co/coffee/sell/controller/sellController.java
+++ coffeemanager/src/main/java/kr/co/coffee/sell/controller/sellController.java
@@ -1,19 +1,53 @@
 package kr.co.coffee.sell.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
+import kr.co.coffee.sell.domain.SellList;
+import kr.co.coffee.sell.service.SellService;
+
+/**
+ * @since 2017-12-26
+ * @author 김영섭
+ * SellController
+ * 
+ */
 @Controller
-
+@RequestMapping(path="/sell")
 public class sellController {
-	@RequestMapping(value="/sell", method=RequestMethod.GET)
-	public String sellPage(Model model) {
-		
+	
+	@Autowired
+	private SellService sellService;
+	/**
+	 * @author 김영섭
+	 * @param model
+	 * @return main
+	 * @throws Exception
+	 * sellService추가하여 초기 디비 생성
+	 * 아래 주석은 실험 처리
+	 * 
+	 */
+	@RequestMapping(method=RequestMethod.GET)
+	public String sellPage(Model model) throws Exception {
+		model.addAttribute("sellList", sellService.getSellList());
 		model.addAttribute("content", "sell/sell_board.jsp");
 		return "main";
 	}
+/*	@RequestMapping(method=RequestMethod.GET)
+	public String sellList(Model model) throws Exception{
+		
+	
+		return "sell/sell_board";
+	}*/
+	
 
 }
