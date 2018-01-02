447d68c47793ed03f1bd0e4884c5b06e0e7e3d3c
Kim Young Seop <kkimm87@gmail.com>
2018-01-02 15:01:50 +0900

    DAO에서 공통 매퍼로 변경
    DAO를 MAPPER로 통일해서 DAO를 없애고 맵퍼가 관리

--- coffeemanager/src/main/java/kr/co/coffee/mapper/CommonMapper.java
+++ coffeemanager/src/main/java/kr/co/coffee/mapper/CommonMapper.java
@@ -3,6 +3,7 @@ package kr.co.coffee.mapper;
 import java.util.List;
 
 import kr.co.coffee.common.domain.Search;
+import kr.co.coffee.menu.domain.MenuVO;
 import kr.co.coffee.sell.domain.SellList;
 import kr.co.coffee.stock.domain.StockList;
 
@@ -11,6 +12,7 @@ public interface CommonMapper {
 	public List<SellList> getSellList(Search search) throws Exception;
 	public List<StockList> selectStockList() throws Exception;
 	public int getTotalCount(Search search) throws Exception;
+	public List<MenuVO>do_searchAll() throws Exception;
 	/*test*/
 	
 }
